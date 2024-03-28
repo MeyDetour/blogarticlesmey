a03543f3c16539db44fa94c8b3791a656f6d0b21
mey <meydetour@gmail.com>
2024-03-28 08:28:53 +0100

    second

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -65,7 +65,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('article-blog@article.com', 'articleblog'))
+                    ->from(new Address('blog@mail.com', 'BLOG MEY'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
