6ff533d262ad2b6c9286ea4d6fb8e17c90a2736a
mey <meydetour@gmail.com>
2024-04-01 14:07:41 +0200

    second

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -65,7 +65,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('blog@mail.com', 'BLOG MEY'))
+                    ->from(new Address('meydetour@zohomail.eu', 'BLOG MEY'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
